ed882ff3f2792da03d0f5f91bc577ad9c842f090
edermelo-git <eder.worksites@gmail.com>
2025-08-02 03:36:43 -0300

    Visual change in the Form

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,30 @@
 import './style.css';
+import Trash from '../../assets/garbage.svg';
 
 function Home() {
 
   return (
 
-      <div className='container_registration'>
+      <div className="container_registration">
         <form>
           <h1>User Registration</h1>
 
-          <input name= "name" type='text'/>
-          <input age= "age" type='number'/>
-          <input email= "e-mail" type='email'/>
-          <button type='button'></button>
+          <input placeholder="Name"name= "name" type='text'/>
+          <input placeholder="Age"age= "age" type='number'/>
+          <input placeholder="e-mail"email= "e-mail" type='email'/>
+          <button type='button'>Register</button>
 
         </form>
         
+        <div>
+          <p>Name:</p>
+          <p>Age:</p>
+          <p>e-mail:</p>
+        </div>
+        <button className='burron trash'>
+          <img src={Trash} alt="Trash icon" />
+        </button>
+
       </div>
   )
 }
