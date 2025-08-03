6b4f4cbbea2e04cbdbefeb00f2f671aa616aa33b
edermelo-git <eder.worksites@gmail.com>
2025-08-03 18:34:20 -0300

    error correction

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,27 +5,27 @@ function Home() {
 
   return (
 
-      <div className="container_registration">
-        <form>
-          <h1>User Registration</h1>
+    <div className="container_registration">
+      <form>
+        <h1>User Registration</h1>
 
-          <input placeholder="Name"name= "name" type='text'/>
-          <input placeholder="Age"age= "age" type='number'/>
-          <input placeholder="e-mail"email= "e-mail" type='email'/>
-          <button type='button'>Register</button>
+        <input placeholder="Name" name="name" type='text' />
+        <input placeholder="Age" age="age" type='number' />
+        <input placeholder="e-mail" email="e-mail" type='email' />
+        <button type='button'>Register</button>
 
-        </form>
-        
-        <div>
-          <p>Name:</p>
-          <p>Age:</p>
-          <p>e-mail:</p>
-        </div>
-        <button className='burron trash'>
-          <img src={Trash} alt="Trash icon" />
-        </button>
+      </form>
 
+      <div>
+        <p>Name:</p>
+        <p>Age:</p>
+        <p>e-mail:</p>
       </div>
+      <button className='button trash'>
+        <img src={Trash} alt="Trash icon" />
+      </button>
+
+    </div>
   )
 }
 
