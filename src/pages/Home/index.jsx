80aca165f1f456e0f5d1f61aca0c91f30b6e7238
edermelo-git <eder.worksites@gmail.com>
2025-08-19 18:26:44 -0300

    add function getUrsers + createUsers

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,47 @@
-import './style.css';
-import Trash from '../../assets/garbage.svg';
-import { useState } from 'react';
+import './style.css'
+import Trash from '../../assets/garbage.svg'
+import { useState, useEffect, useRef } from 'react'
+import api from '../../services/api'
 
 function Home() {
   const [users, setUsers] = useState([]);
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+
+  async function getUsers() {
+    try {
+      const response = await api.get('/users');
+      setUsers(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function createUrsers() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+      
+    })
+
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <div className="container_registration">
       <form>
         <h1>User Registration</h1>
-
-        <input placeholder="Name" name="name" type="text" />
-        <input placeholder="Age" name="age" type="number" />
-        <input placeholder="E-mail" name="email" type="email" />
-        <button type="button">Register</button>
+        <input placeholder="Name" name="name" type="text" ref={inputName} />
+        <input placeholder="Age" name="age" type="number" ref={inputAge} />
+        <input placeholder="E-mail" name="email" type="email" ref={inputEmail} />
+        <button type="button" onClick={createUrsers}>Register</button>
       </form>
 
       {users.map(user => (
